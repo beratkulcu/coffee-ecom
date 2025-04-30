00cad355606062538fd00c49bb8b96f2bac888d5
Berat Kulcu <beratkulcu@192.168.1.41>
2025-04-30 19:35:50 +0300

    Initial project setup: monorepo structure, auth-service base, Docker infra

---
+++ src/test/java/com/coffecommerce/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffecommerce.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
